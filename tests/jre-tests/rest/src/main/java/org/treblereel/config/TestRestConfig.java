1fc0f963ea42695caee2a9aa36d9c2897ae861d4
Dmitrii Tikhomirov <chani.liet@gmail.com>
2026-06-30 11:56:25 -0700

    feat(rest-tests): scaffold REST integration test module with source code

---
+++ tests/jre-tests/rest/src/main/java/org/treblereel/config/TestRestConfig.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2020 Treblereel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.treblereel.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Produces;
+
+import org.treblereel.gwt.rest.client.RestConfig;
+import org.treblereel.transport.JdkTransport;
+
+@Alternative
+@ApplicationScoped
+public class TestRestConfig {
+
+    public static String BASE_URL = "http://localhost:8080";
+
+    @Produces
+    public RestConfig restConfig() {
+        return RestConfig.builder()
+                .baseUrl(BASE_URL)
+                .transport(new JdkTransport())
+                .build();
+    }
+}
