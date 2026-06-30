6fa84c69c29dafd3fc3d25413a549b1ca5479ac8
Dmitrii Tikhomirov <chani.liet@gmail.com>
2026-06-29 18:27:23 -0700

    feat(rest): add RestCallerGenerator for @Inject Caller<T>

---
+++ ui/rest/generator/src/main/java/io/crysknife/ui/rest/generator/RestCallerGenerator.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2026 Treblereel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.crysknife.ui.rest.generator;
+
+import jakarta.inject.Inject;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
+
+import com.github.javaparser.ast.expr.FieldAccessExpr;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.expr.ObjectCreationExpr;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import io.crysknife.definition.BeanDefinition;
+import io.crysknife.definition.InjectableVariableDefinition;
+import io.crysknife.generator.api.Generator;
+import io.crysknife.generator.api.IOCGenerator;
+import io.crysknife.generator.api.WiringElementType;
+import io.crysknife.generator.context.IOCContext;
+import io.crysknife.logger.TreeLogger;
+import org.treblereel.gwt.rest.client.Caller;
+import org.treblereel.gwt.rest.client.RestConfig;
+
+@Generator(priority = 100001)
+public class RestCallerGenerator extends IOCGenerator<BeanDefinition> {
+
+    public RestCallerGenerator(TreeLogger treeLogger, IOCContext iocContext) {
+        super(treeLogger, iocContext);
+    }
+
+    @Override
+    public void register() {
+        iocContext.register(Inject.class, Caller.class, WiringElementType.FIELD_TYPE, this);
+        iocContext.getBuildIn().add(Caller.class.getCanonicalName());
+    }
+
+    @Override
+    public String generateBeanLookupCall(InjectableVariableDefinition fieldPoint) {
+        DeclaredType declaredType = (DeclaredType) fieldPoint.getVariableElement().asType();
+        TypeMirror typeArg = declaredType.getTypeArguments().get(0);
+        String callerImplQualifiedName = typeArg.toString() + "_RestCaller";
+
+        MethodCallExpr lookupRestConfig = new MethodCallExpr(
+                new MethodCallExpr(new NameExpr("beanManager"), "lookupBean")
+                        .addArgument(new FieldAccessExpr(
+                                new NameExpr(RestConfig.class.getCanonicalName()), "class")),
+                "getInstance");
+
+        ObjectCreationExpr newCaller = new ObjectCreationExpr()
+                .setType(new ClassOrInterfaceType(null, callerImplQualifiedName))
+                .addArgument(lookupRestConfig);
+
+        return generationUtils.wrapCallInstanceImpl(newCaller).toString();
+    }
+}
